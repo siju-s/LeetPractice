6e9a73482e0d89811161d62d40aed7bf736fdae2
Siju S <sijus887@gmail.com>
2024-05-15 00:10:35 -0500

    Time: 269 ms (41.11%) | Memory: 25.8 MB (26.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,38 @@
 class Solution:
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     leftProdArr = [1] * len(nums)
+    #     rightProdArr = [1] * len(nums)
+
+    #     prod = 1
+    #     for i in range(1, len(nums)):
+    #         leftProdArr[i] = prod * nums[i - 1]
+    #         prod = leftProdArr[i]
+
+    #     prod = 1
+    #     for j in range(len(nums) - 2, -1, -1):
+    #         rightProdArr[j] = prod * nums[j + 1]
+    #         prod = rightProdArr[j]
+
+    #     output = [0] * len(nums)
+
+    #     for i in range(0, len(nums)):
+    #         output[i] = leftProdArr[i] * rightProdArr[i]
+
+    #     return output    
+
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        leftProdArr = [1] * len(nums)
-        rightProdArr = [1] * len(nums)
+        output = [1] * len(nums)
 
-        prod = 1
-        for i in range(1, len(nums)):
-            leftProdArr[i] = prod * nums[i - 1]
-            prod = leftProdArr[i]
+        leftProd = 1
+        for i in range(len(nums)):
+            output[i] *= leftProd
+            leftProd *= nums[i] 
 
-        prod = 1
-        for j in range(len(nums) - 2, -1, -1):
-            rightProdArr[j] = prod * nums[j + 1]
-            prod = rightProdArr[j]
+        rightProd = 1
+        for j in range(len(nums) - 1, -1, -1):
+            output[j] *= rightProd
+            rightProd *= nums[j]
 
-        output = [0] * len(nums)
-
-        for i in range(0, len(nums)):
-            output[i] = leftProdArr[i] * rightProdArr[i]
 
         return output    
-
-
         
