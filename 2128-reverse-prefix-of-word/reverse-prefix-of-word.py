4c0cf5cf4ed34a9461af0839c7779f6c139fb909
Siju S <sijus887@gmail.com>
2024-06-22 12:17:56 -0500

    Time: 38 ms (34.26%) | Memory: 16.4 MB (99.23%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        j = word.find(ch)
+        if j != -1:
+            return word[j::-1] + word[j+1:]
+        return word
+
+        
