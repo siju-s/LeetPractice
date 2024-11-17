92f8a65bb5cf7fff870cf8ba7775f3a9a6a80741
Siju S <sijus887@gmail.com>
2024-11-16 22:05:35 -0600

    Time: 0 ms (100.00%) | Memory: 11.6 MB (33.69%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,26 +1,30 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i, j, p = m - 1, n - 1, m + n - 1
-        while i >= 0 and j >= 0:
-            if (nums1[i] > nums2[j]):
-                nums1[p] = nums1[i]
-                i -= 1
-            else:
-                nums1[p] = nums2[j]
-                j -= 1
-            print(nums1[p])
-            p -= 1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
-        while i >= 0:
-            nums1[p] = nums1[i]
-            i -= 1
-            p -= 1
+        while i >= 0 and j >= 0:
+            if nums1[i] <= nums2[j]:
+               nums1[k] = nums2[j]
+               j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+
+            k -= 1
 
         while j >= 0:
-            nums1[p] = nums2[j]
-            j -= 1
-            p -= 1            
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1    
+
+
         
